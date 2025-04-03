3116641619a53d36cd90e36425e038bad548be40
Ethan Brown <eb934053@gmail.com>
2025-04-03 15:22:17 -0500

    Changed order properties to access keys on API response. Swapped API URL in order.js

--- components/order/detail.js
+++ components/order/detail.js
@@ -2,16 +2,16 @@ import Table from "../table"
 
 export default function CartDetail({ cart, removeProduct }) {
   const headers = ['Product', 'Price', '']
-  const footers = ['Total', cart.total, '']
+  const footers = ['Total ', '$' + (cart.total_cost || 0), '']
 
   return (
     <Table headers={headers} footers={footers}>
       {
-        cart.products?.map(product => {
+        cart.lineitems?.map(lineitem => {
           return (
-            <tr key={product.id}>
-              <td>{product.name}</td>
-              <td>{product.price}</td>
+            <tr key={lineitem.id}>
+              <td>{lineitem.product.name}</td>
+              <td>${lineitem.product.price}</td>
               <td>
                 <span className="icon is-clickable" onClick={() => removeProduct(product.id)}>
                   <i className="fas fa-trash"></i>
