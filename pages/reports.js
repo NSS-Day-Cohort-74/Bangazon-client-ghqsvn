b44ce08b1df6c854dd5cf15d50bb694be7edfa94
Brian Henry <brianhenrytech@gmail.com>
2025-04-17 16:53:06 -0500

    Added favorite sellers report hanlding

--- pages/reports.js
+++ pages/reports.js
@@ -1,39 +1,98 @@
-import React, { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Layout from "../components/layout";
 import Navbar from "../components/navbar";
 import Link from "next/link";
 
 const Reports = () => {
-  const [reports, setReports] = React.useState([]);
-  const auth =
-    typeof window !== "undefined"
-      ? `?token=${localStorage.getItem("token")}`
-      : `?token=${localStorage.getItem("token")}`;
+  const [reports, setReports] = useState([]);
+  const [customers, setCustomers] = useState([]);
 
+  const getAuthParam = (url = "") => {
+    const prefix = url.includes("?") ? "&" : "?";
+    const token = localStorage.getItem("token");
+    if (url.includes("customer")) {
+      return token ? `1${prefix}token=${token}` : "";
+    }
+    return token ? `${prefix}token=${token}` : "";
+  };
   useEffect(() => {
-    const fetchReports = async () => {
+    const fetchData = async (url, setter) => {
       try {
-        const response = await fetch(`http://localhost:8000/reports${auth}`);
+        const response = await fetch(url);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        setReports(data);
+        setter(data);
       } catch (error) {
         console.error("Error fetching reports:", error);
       }
     };
-    fetchReports();
+    fetchData(`http://localhost:8000/reports${getAuthParam()}`, setReports);
+    // fetchData(`http://localhost:8000/customers`, setCustomers);
   }, []);
 
   return (
     <div className="container">
-      <h1 className="title">Bangazon Reports</h1>
+      <h1 className="title has-text-centered mt-5">Bangazon Reports</h1>
       <ul className="box">
-        {reports.map((report) => (
-          <li key={report.id} className="card p-4 m-2">
-            <Link href={`${report.url}${auth}`}>{report.title}</Link>
-          </li>
+        {reports.map(({ id, url, title }) => (
+          <div key={id} className="mb-4">
+            {!url.includes("customer=") ? (
+              <li className="card p-4 m-2">
+                <Link href={`${url}${getAuthParam(url)}`}>
+                  <button className="button is-primary is-fullwidth">
+                    {title}
+                  </button>
+                </Link>
+              </li>
+            ) : (
+              <div className="card p-4 m-2">
+                <button
+                  className="button is-primary is-fullwidth mb-3"
+                  disabled
+                >
+                  <Link href={`${url}${getAuthParam(url)}`}>{title}</Link>
+                </button>
+                <div className="dropdown is-hoverable">
+                  <div className="dropdown-trigger">
+                    <button
+                      className="button is-light is-fullwidth"
+                      aria-haspopup="true"
+                      aria-controls="dropdown-menu"
+                    >
+                      <span>Select Customer</span>
+                    </button>
+                  </div>
+                  <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                    <div className="dropdown-content p-3">
+                      <label className="label">Customer ID:</label>
+                      <input
+                        type="number"
+                        className="input is-small mb-2"
+                        placeholder="Enter Customer ID"
+                      />
+                      <button
+                        className="button is-link is-small is-fullwidth"
+                        onClick={({
+                          target: {
+                            previousSibling: { value },
+                          },
+                        }) => {
+                          if (value)
+                            window.location.href = `${url}${value}${getAuthParam(
+                              url
+                            )}`;
+                        }}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         ))}
       </ul>
     </div>
